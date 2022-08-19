935956cba7b80d2f96a59a63e0ae2e324dec2f9c
AdrianTejada <atejada@my.bcit.ca>
2022-08-19 16:01:57 -0700

    Added index.scss and testing responsiveness

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Button from './Components/Button';
 import Card from './Components/Card';
+import './Styles/index.css';
 
 function App() {
   return (
     <div>
       <Button text='hover on me'/>
       <Card header='hello this is header' text='hello this is text'/>
+      <div className='responsive-test'>responsive test</div>
     </div>
   );
 }
